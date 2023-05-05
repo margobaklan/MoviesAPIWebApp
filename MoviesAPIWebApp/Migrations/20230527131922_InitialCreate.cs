c8bc306fcd421a41e6ea28fa223e34e41074f674
margobaklan <margo.baklan@gmail.com>
2023-05-05 23:23:35 +0300

    измененияИзменения.

---
+++ MoviesAPIWebApp/Migrations/20230527131922_InitialCreate.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoviesAPIWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Directors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DirectorMovies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    DirectorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DirectorMovies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DirectorMovies_Directors_DirectorId",
+                        column: x => x.DirectorId,
+                        principalTable: "Directors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DirectorMovies_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DirectorMovies_DirectorId",
+                table: "DirectorMovies",
+                column: "DirectorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DirectorMovies_MovieId",
+                table: "DirectorMovies",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_GenreId",
+                table: "Movies",
+                column: "GenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DirectorMovies");
+
+            migrationBuilder.DropTable(
+                name: "Directors");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
---
+++ MoviesAPIWebApp/wwwroot/js/lab2.js
@@ -0,0 +1,109 @@
+﻿const uri = 'api/Genres';
+let genres = [];
+
+function getGenres() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayGenres(data))
+        .catch(error => console.error('Unable to get genres.', error));
+}
+
+function addGenre() {
+    const addNameTextbox = document.getElementById('add-name');
+    const addInfoTextbox = document.getElementById('add-desctiption');
+    const genre = {
+        name: addNameTextbox.value.trim(),
+        description: addInfoTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(genre)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getGenres();
+            addNameTextbox.value = '';
+            addInfoTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add genre.', error));
+}
+
+function deleteGenre(id) {
+    fetch(`${uri}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(() => getGenres())
+        .catch(error => console.error('Unable to delete genre.', error));
+}
+
+function displayEditForm(id) {
+    const genre = genres.find(genre => genre.id === id);
+    document.getElementById('edit-id').value = genre.id;
+    document.getElementById('edit-name').value = genre.name;
+    document.getElementById('edit-desctiption').value = genre.description;
+    document.getElementById('editForm').style.display = 'block';
+}
+
+function updateGenre() {
+    const genreId = document.getElementById('edit-id').value;
+    const genre = {
+        id: parseInt(genreId, 10),
+        name: document.getElementById('edit-name').value.trim(),
+        description: document.getElementById('edit-desctiption').value.trim()
+    };
+
+    fetch(`${uri}/${genreId}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(genre)
+    })
+        .then(() => getGenres())
+        .catch(error => console.error('Unable to update genre.', error));
+    closeInput();
+    return false;
+}
+
+function closeInput() {
+    document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayGenres(data) {
+    const tBody = document.getElementById('genres');
+    tBody.innerHTML = '';
+    const button = document.createElement('button');
+
+    data.forEach(genre => {
+        let editButton = button.cloneNode(false);
+        editButton.innerText = 'Edit';
+        editButton.setAttribute('onclick', `displayEditForm(${genre.id})`);
+
+        let deleteButton = button.cloneNode(false);
+        deleteButton.innerText = 'Delete';
+        deleteButton.setAttribute('onclick', `deleteGenre(${genre.id})`);
+
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        let textNode = document.createTextNode(genre.name);
+        td1.appendChild(textNode);
+
+        let td2 = tr.insertCell(1);
+        let textNodeInfo = document.createTextNode(genre.description);
+        td2.appendChild(textNodeInfo);
+
+        let td3 = tr.insertCell(2);
+        td3.appendChild(editButton);
+
+        let td4 = tr.insertCell(3);
+        td4.appendChild(deleteButton);
+    });
+    genres = data;
+}
